9f4e674964aa9950f9e6f39f1e1e91a530b53883
Kasper Osterbye <kasper@cphbusiness.dk>
2016-09-20 07:17:59 +0200

    cleaning up the demo folder

--- demo/day2BinarySearchTreeDemo/src/binarysearchtreedemo/BinaryTreeApp.java
+++ demo/day2BinarySearchTreeDemo/src/binarysearchtreedemo/BinaryTreeApp.java
@@ -1,25 +1,31 @@
 package binarysearchtreedemo;
 
-public class BinaryTreeApp
-{
 
-    public static void main(String[] args)
-    {
-        BinaryTree bt = new BinaryTree();
+public class BinaryTreeApp {
 
-        bt.insert(2);
-        bt.insert(1);
-        bt.insert(3);
-
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BinaryTree bt = new BinaryTree();
+                
+                bt.insert(2);
+                bt.insert(1);
+                bt.insert(3);
+                   
+			
+		
+		
 //		bt.printPostorder();
-        boolean res = bt.lookup(2);
-        System.out.println(res);
+		boolean res = bt.lookup(2);                
+		System.out.println(res);
+                
+                res = bt.lookup(4); 
+                System.out.println(res);
 
-        res = bt.lookup(4);
-        System.out.println(res);
+		//int min = bt.minValue();
+		//System.out.println(min);
 
-        //int min = bt.minValue();
-        //System.out.println(min);
-    }
+	}
 
 }
